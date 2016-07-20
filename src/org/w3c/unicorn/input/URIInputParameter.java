b2a7750ab8a351ce3cb15712c2479c907fc8516c
jeremyweber <jeremyweber@192.168.0.34>
2016-07-20 14:39:48 -0400

    Use GET instead of HEAD

--- src/org/w3c/unicorn/input/URIInputParameter.java
+++ src/org/w3c/unicorn/input/URIInputParameter.java
@@ -103,7 +103,7 @@ public class URIInputParameter extends InputParameter {
 			HttpURLConnection con = (HttpURLConnection) docUrl.openConnection();
 			con.setRequestProperty("User-agent", "W3C_Unicorn/1.0 (http://validator.w3.org/services)");
 			con.setConnectTimeout(connectTimeOut);
-			con.setRequestMethod("HEAD");
+			con.setRequestMethod("GET");
 			try {
 				con.connect();
 			} catch (SSLException e) {
